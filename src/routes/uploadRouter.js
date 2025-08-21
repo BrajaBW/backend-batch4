45c8e1de60f0a2a80dabcd42aa03a9dddf686b5d
brajaBW <brajabuanawidodo93@gmail.com>
2025-08-21 20:32:46 +0700

    fix errro multer storage

--- src/routes/uploadRouter.js
+++ src/routes/uploadRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const multer = require('multer');
+const storage = multer.memoryStorage();
 const uploadController = require('../controllers/uploadController')
 
 const router = express.Router();
+const update = multer({storage})
 const upload = multer({ dest: 'uploads/' });
+
 
 router.post('/', upload.single('gambar'), uploadController.uploadFile)
 
